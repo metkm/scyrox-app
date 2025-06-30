6b7bb0694b71a2d3ff39954be6e2c4f9fe8eb4ef
metkm <metinkorkmaz417@gmail.com>
2025-06-30 21:31:24 +0300

    can read device now

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,13 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+mod device;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .setup(|app| {
+            let device = device::get_device();
+            println!("{:?}", device);
+
             #[cfg(debug_assertions)] // only include this code on debug builds
             {
                 use tauri::Manager;

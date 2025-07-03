99a2b638777b7c27f039173768ed876a7ed11bd0
metkm <metinkorkmaz417@gmail.com>
2025-07-03 22:57:33 +0300

    move closures to their own function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,78 @@
 use std::sync::Mutex;
 
 use tauri::{
-    Manager, WindowEvent,
-    menu::{Menu, MenuItem},
-    tray::{MouseButton, TrayIconBuilder, TrayIconEvent},
+    menu::{Menu, MenuItem}, tray::{MouseButton, TrayIconBuilder, TrayIconEvent}, App, Manager, Window, WindowEvent
 };
 use tauri_plugin_store::StoreExt;
 
 mod commands;
 mod device;
 mod models;
+
+pub fn handle_window_event(window: &Window, event: &WindowEvent) {
+    if let WindowEvent::CloseRequested { api, .. } = event {
+        let app_handle = window.app_handle();
+
+        let minimize_to_tray = app_handle.store("config.json")
+            .and_then(|store| {
+                Ok(
+                    store.get("minimize_to_tray")
+                        .and_then(|value| value.as_bool())
+                        .unwrap_or(false)
+                )
+            })
+            .unwrap_or(false);
+
+        if minimize_to_tray {
+            window.hide().unwrap();
+            api.prevent_close();
+        }
+    }
+}
+
+pub fn handle_setup(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
+    let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+    let menu = Menu::with_items(app, &[&quit_i]).unwrap();
+
+    let _tray = TrayIconBuilder::with_id("tray_icon_battery")
+        .icon(app.default_window_icon().unwrap().clone())
+        .on_menu_event(|app, event| match event.id.as_ref() {
+            "quit" => app.exit(0),
+            _ => {}
+        })
+        .show_menu_on_left_click(false)
+        .on_tray_icon_event(|icon, event| {
+            if let TrayIconEvent::Click {
+                button: MouseButton::Left,
+                ..
+            } = event
+            {
+                let app_handle = icon.app_handle();
+                let window = app_handle.get_webview_window("main").unwrap();
+                window.show().ok();
+            };
+        })
+        .menu(&menu)
+        .build(app)?;
+
+    let device = device::get_device();
+    let state = Mutex::new(models::AppState {
+        device,
+        ..Default::default()
+    });
+
+    app.manage(state);
+
+    #[cfg(debug_assertions)] // only include this code on debug builds
+    {
+        use tauri::Manager;
+
+        let window = app.get_webview_window("main").unwrap();
+        window.open_devtools();
+    }
+
+    Ok(())
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -28,69 +91,8 @@ pub fn run() {
             commands::app::get_minimize_to_tray
         ])
         .plugin(tauri_plugin_store::Builder::default().build())
-        .setup(|app| {
-            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
-            let menu = Menu::with_items(app, &[&quit_i]).unwrap();
-
-            let _tray = TrayIconBuilder::with_id("tray_icon_battery")
-                .icon(app.default_window_icon().unwrap().clone())
-                .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => app.exit(0),
-                    _ => {}
-                })
-                .show_menu_on_left_click(false)
-                .on_tray_icon_event(|icon, event| {
-                    if let TrayIconEvent::Click {
-                        button: MouseButton::Left,
-                        ..
-                    } = event
-                    {
-                        let app_handle = icon.app_handle();
-                        let window = app_handle.get_webview_window("main").unwrap();
-                        window.show().ok();
-                    };
-                })
-                .menu(&menu)
-                .build(app)?;
-
-            let device = device::get_device();
-            let state = Mutex::new(models::AppState {
-                device,
-                ..Default::default()
-            });
-
-            app.manage(state);
-
-            #[cfg(debug_assertions)] // only include this code on debug builds
-            {
-                use tauri::Manager;
-
-                let window = app.get_webview_window("main").unwrap();
-                window.open_devtools();
-            }
-
-            Ok(())
-        })
-        .on_window_event(|window, event| {
-            if let WindowEvent::CloseRequested { api, .. } = event {
-                let app_handle = window.app_handle();
-
-                let minimize_to_tray = app_handle.store("config.json")
-                    .and_then(|store| {
-                        Ok(
-                            store.get("minimize_to_tray")
-                                .and_then(|value| value.as_bool())
-                                .unwrap_or(false)
-                        )
-                    })
-                    .unwrap_or(false);
-
-                if minimize_to_tray {
-                    window.hide().unwrap();
-                    api.prevent_close();
-                }
-            }
-        })
+        .setup(handle_setup)
+        .on_window_event(handle_window_event)
         .run(tauri::generate_context!())
         .expect("error while running tauri application")
 }
